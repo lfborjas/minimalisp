39705349fa96416523257e3f2d827e4ab093701f
lfborjas <luis.borjas@escolarea.com>
2011-03-14 21:55:56 -0600

    made a mess with open classes and stuff, testing

--- norvig.rb
+++ norvig.rb
@@ -8,15 +8,58 @@ class Env < Hash
         @outer = outer
     end
     def find(v)
+        v = v.to_sym
         self.include?(v) ? self : @outer.find(v)
     end
     alias_method(:_get, :[]) unless method_defined? :_get
     def [](k)
-        _get[k.to_sym]
+        _get k.to_sym
     end
 end
 
-def add_globals(e)
+
+class Object
+    def atom?
+        return true if self.is_a? String
+        !self.respond_to? :each
+    end
+
+    def to_atom
+        Integer(self) rescue Float(self) rescue to_sym
+    end
+
+    def to_lisp
+        self.is_a? Array ? "(#{self.collect{|v| v.to_lisp}.join(' ')})" : self.to_s
+    end
+end
+
+class String
+    def ==(o)
+        return self.to_sym.eql?(o) if o.is_a? Symbol
+        eql? o
+    end
+
+    def to_sexp
+        read_from(self.gsub(/([()])/){|m| " #{m} "}.split)
+    end
+
+    private
+        def read_from(tokens)
+            raise "Unexpected EOF while reading" unless tokens.length > 0
+            token = tokens.shift
+            case token
+                when '(' 
+                    l = []
+                    l << read_from(tokens) until tokens.first == ")"
+                    tokens.shift
+                    l
+                when ')' then raise "Syntax error: unexpected ')'"
+                else token.to_atom
+            end
+        end
+end
+
+Kernel::Global_env = Env.new.instance_eval do |e|
     #add the math methods:
     Math.__module_init__.each{|m| e[m] = lambda{|*args| Math.send(m, *args)}}
 
@@ -40,30 +83,47 @@ def add_globals(e)
     e[:list?]  = lambda{|x| x.is_a? Array}
     e[:null?]  = lambda{|x| x==[]}
     e[:symbol?]= lambda{|x| x.is_a?(Symbol)}
-end
-
-global_env = add_globals(Env.new)
-
-class Object
-    def atom?
-        return true if self.is_a? String
-        !self.respond_to? :each
-    end
-end
-
-class String
-    def ==(o)
-        return self.to_sym.eql?(o) if o.is_a? Symbol
-        eql? o
-    end
+    e
 end
 
 def eval!(x, env)
-    if    x.atom?         then env.find(x)[x] #variable reference
-    elsif !x.is_a?(Array) then x              #constant literal
-    elsif x.first == :quote  then _, exp = x ; exp
-    elsif x.first == :if     then _, t, c, a = x; eval!(eval!(t, env)?c : t, env)
-    elsif x.first == :set!   then _, v, exp = x ; env.find(v)[v] = eval!(exp, env)
-    elsif x.first == :define then _, v, exp = x ; env[v] = eval!(exp, env)
-    elsif x.first == :lambda then 
+    if    x.atom?         
+        env.find(x)[x] 
+    elsif !x.is_a?(Array)
+        x              
+    else
+        case x.first
+         when :quote 
+             _, exp = x
+             exp
+         when :if
+             _, test, conseq, alt = x
+             eval!(eval!(test, env)? conseq : alt, env)
+         when :set!
+             _, v, exp = x
+             env.find(v)[v] = eval!(exp, env)
+         when :define
+             _, v, exp = x
+             env[v] = eval!(exp, env)
+         when :lambda 
+             _, vars, exp = x
+             lambda {|*args| eval!(exp, Env.new(vars, args, env))}
+         when :begin
+             val = nil
+             x.slice(1..-1).each{|exp| val = eval!(exp, env) } 
+             val
+         else #it's some other function
+             exps = x.collect{|exp| eval!(exp, env)}
+             fun  = exps.shift
+             fun.call *exps
+        end
+    end
 end
+
+if __FILE__ == $0
+    require 'readline'
+    while exp = Readline.readline("screem > ", true)
+        puts "=> #{eval!(exp.to_sexp, Global_env)}"
+    end
+end
+
