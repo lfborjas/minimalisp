682f5e54d0ae5e32f149767ac4cb37f7bb6c25ff
lfborjas <luis.borjas@escolarea.com>
2011-04-22 21:45:55 -0600

    changed tests

--- test/jmc_lisp/test/core.clj
+++ test/jmc_lisp/test/core.clj
@@ -5,42 +5,36 @@
 ;Test stuff without an environ
 (are [exp res] (= (eval! exp '()) res)
      '(quote x) 'x
-     '(atom? 1) true
-     '(atom? (1 2)) false
-     '(eq? 2 2) true
-     '(eq? 3 4) true
-     '(car (quote (1 2 3))) 1
-     '(cdr (quote (1 2 3))) '(2 3)
+     '(coll? 1) false
+     '(coll? (1 2)) true
+     '(= 2 2) true
+     '(= 3 4) true
+     '(first (quote (1 2 3))) 1
+     '(rest (quote (1 2 3))) '(2 3)
      '(cons 1 (quote (2 3))) '(1 2 3)
-     '(cond ((eq? 2 2) true) (true false)) true 
-     '(cond ((eq? 2 3) false) (true true)) true
-     '((lambda (x) (atom? x)) 1) true
-     '((label l (lambda (x) (eq? x 2))) 2) true)
+     '(cond (eq? 2 2) true true false) true 
+     '(cond (eq? 2 3) false true true) true
+     '((fn [x] (coll? x)) 1) false
+     '((def f (fn [x] (= x 2))) 2) true)
 
 ;Test stuff with an environ
 (are [exp env res] (= (eval! exp env) res)
-     '(null? (quote ())) '((null? (lambda (l) (eq? l (quote ()))))) true
-     '(null? (quote (1 2))) '((null? (lambda (l) (eq? l (quote ()))))) false 
-     '((label cadar 
-             (lambda (s) 
-                     (car (cdar s)))) (quote (((1 2) 3) 4))) '((cdar (lambda (l) (cdr (car l))))) 3
-     '((label append
-              (lambda (x y)
+     '(empty? (quote ())) '((empty? (fn [l] (= l (quote ()))))) true
+     '(empty? (quote (1 2))) '((empty? (fn [l] (= l (quote ()))))) false 
+     '((def cadar 
+             (fn [s] 
+                     (first (cdar s)))) (quote (((1 2) 3) 4))) '((cdar (fn [l] (rest (first l))))) 3
+     '((def append
+              (fn [x y]
                       (cond 
-                        ((null? x) y)
-                        (true (cons (car x) (append (cdr x) y))))))
-         (quote (1 2 3)) (quote (4 5 6))) '((null? (lambda (l) (eq? l (quote ()))))) '(1 2 3 4 5 6)
-     '((label maplist
-              (lambda (x p)
+                        (empty? x) y
+                        true (cons (first x) (append (rest x) y)))))
+         (quote (1 2 3)) (quote (4 5 6))) '((empty? (fn [l] (= l (quote ()))))) '(1 2 3 4 5 6)
+     '((def maplist
+              (fn [x p]
                       (cond 
-                        ((null? x) (quote ()))
-                        (true (cons (p x) (maplist (cdr x) p)))))) 
-         (quote (1 2 3 2)) (quote (lambda (e) (eq? e 2)))) '((null? 
-                                                                   (lambda (l)
-                                                                           (eq? l (quote ()))))) '(false true false true)) 
-
-
-
-
-
-
+                        (empty? x) (quote ())
+                        true (cons (p x) (maplist (rest x) p))))) 
+         (quote (1 2 3 2)) (quote (fn [e] (= e 2)))) '((empty? 
+                                                           (fn [l]
+                                                                   (= l (quote ()))))) '(false true false true)) 
